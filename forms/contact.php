d72d2a8c9bed30b216ff62febbda18788c8533bb
AjayIsrani <ajayisrani3@gmail.com>
2025-01-02 01:51:27 +0530

    Update contact.php
    Update set html template in email body

--- forms/contact.php
+++ forms/contact.php
@@ -4,7 +4,7 @@
   $email = $_POST['email'];
   $subject = $_POST['subject'];
   $message = $_POST['message'];
-  $phoneno = $_POST['phone'];
+  $subject = $_POST['subject'];
   
   ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
@@ -13,11 +13,28 @@
     $subject = $subject;
     $message = 'Name : ' . $name . '
     Contact Mail : ' . $email . '
-    Contact No : ' . $phoneno . '
+    Subject : ' . $subject . '
     Message : ' . $message;
     $headers = "From:" . $from;
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    $message = '<div><p>' . $name . ' just submitted our home page form on <a href="https://vardhmanchemicalindustries.com" target="_blank">https://vardhmanchemicalindustries.com</a>.</p>
+                    <p>Here''s what they had to say:</p>
+                    <strong>Name : </strong><pre style="margin:0;white-space:pre-wrap">' . $name . '</pre>
+                    <hr>
+                    <strong>Contact Mail : </strong><pre style="margin:0;white-space:pre-wrap">' . $email . '</pre>
+                    <hr>
+                    <strong>Subject :</strong><pre style="margin:0;white-space:pre-wrap">' . $subject . '</pre>
+                    <hr>
+                    <strong>Message : </strong><pre style="margin:0;white-space:pre-wrap">' . $message . '</pre>
+                    <hr>
+              <p style="text-align:center">Submitted at '. new DateTime('NOW', new DateTimeZone('UTC')) .'</p>
+          <br>
+        <p>Our Team,</p>
+        <a style="text-decoration:none" href="https://vardhmanchemicalindustries.com" target="_blank"><strong><span class="il">Vardhman Chemical Industries</span> Team</strong></a>
+       </div>'
     if(mail($to,$subject,$message, $headers)) {
-    echo "The email message was sent.";
+    echo "OK";
     } else {
       echo "The email message was not sent.";
     }
